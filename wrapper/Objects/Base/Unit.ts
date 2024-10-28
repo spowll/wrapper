96e9c88ccbd55994c0c70ca932451b24cc5442d8
SicdeX <sicdex@gmail.com>
2024-10-29 04:41:40 +0600

    see desc
    - remove requestUnitUpdate
    - remove total move speed

--- wrapper/Objects/Base/Unit.ts
+++ wrapper/Objects/Base/Unit.ts
@@ -176,7 +176,6 @@ export class Unit extends Entity {
 	public TotalAgility: number = 0
 	public BaseTotalIntellect: number = 0
 	public TotalStrength: number = 0
-	public MoveSpeedTotal: number = 0
 	public AttackCapabilities: number = 0
 	public UnitStateNetworked: bigint = 0n
 
@@ -426,7 +425,7 @@ export class Unit extends Entity {
 		// const totalSpeed = isLimit
 		// 	? Math.min(MoveSpeedData.Max, Math.max(MoveSpeedData.Min, calculateSpeed))
 		// 	: Math.max(MoveSpeedData.Min, calculateSpeed)
-		return this.MoveSpeedTotal
+		return 0
 	}
 	/** ============================== Turn Rate ======================================= */
 	public get BaseMovementTurnRateData(): number {
@@ -1148,12 +1147,8 @@ export class Unit extends Entity {
 	 * @internal
 	 * @deprecated
 	 */
-	public ForwardNativeProperties(
-		healthBarOffset: number,
-		totalMoveSpeed: number
-	): void {
+	public ForwardNativeProperties(healthBarOffset: number): void {
 		this.HealthBarOffset_ = healthBarOffset
-		this.MoveSpeedTotal = totalMoveSpeed
 	}
 	/**
 	 * @param fromCenterToCenter include HullRadiuses (for Units)
@@ -1349,7 +1344,7 @@ export class Unit extends Entity {
 			ar.push(asMod[1])
 		}
 
-		const moveSpeed = this.MoveSpeedTotal
+		const moveSpeed = this.Speed
 		const msMod = this.UnitData.MovementSpeedActivityModifiers.find(
 			mod => moveSpeed >= mod[0]
 		)
